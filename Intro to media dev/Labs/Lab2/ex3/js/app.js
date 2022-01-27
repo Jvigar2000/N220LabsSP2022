02ac59de58f91b4d31eac4be3fb885355f0b10e2
Jvigar2000 <jameson.vigar@aol.com>
2022-01-27 11:53:45 -0500

    Html update

--- Intro to media dev/Labs/Lab2/ex3/js/app.js
+++ Intro to media dev/Labs/Lab2/ex3/js/app.js
@@ -1,4 +1,35 @@
 // Jameson Vigar
 // 1/26/22
 // N220
-// 
+// World Warp
+
+x = 1;
+
+function setup(){
+
+    createCanvas(800,600);
+
+    fill("black");
+
+}
+
+function draw(){
+
+   
+
+    if (frameCount % 1 == 0 ) { 
+        background("white");
+           x ++;
+        circle(x, 300, 50);
+       }
+    
+
+    if(x == 800){
+        
+        x = 1;
+
+    }
+
+
+
+}
