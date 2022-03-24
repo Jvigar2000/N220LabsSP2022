e3e5feeea8044f7712aee40230d9cb6dc1e51897
Jvigar2000 <jameson.vigar@aol.com>
2022-03-24 16:51:00 -0400

    bug fixes

--- Intro to media dev/Labs/InputEx/distanceCalc/js/app.js
+++ Intro to media dev/Labs/InputEx/distanceCalc/js/app.js
@@ -12,8 +12,8 @@ let Y2 = document.getElementById("y2");
 function distance(){
 
     // calculate the differences
-    let xDiff = X1 - X2;
-    let yDiff = Y1 - Y2;
+    let xDiff = X1.value - X2.value;
+    let yDiff = Y1.value - Y2.value;
 
     // distance equation
     let d = Math.sqrt( xDiff*xDiff + yDiff*yDiff);
