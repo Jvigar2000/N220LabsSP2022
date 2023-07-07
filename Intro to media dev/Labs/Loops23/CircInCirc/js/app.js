4b5982426eba4550ec244c9fc53fd095bd461aee
Jvigar2000 <jameson.vigar@aol.com>
2023-07-07 14:53:34 -0400

    circle

--- Intro to media dev/Labs/Loops23/CircInCirc/js/app.js
+++ Intro to media dev/Labs/Loops23/CircInCirc/js/app.js
@@ -19,13 +19,13 @@ function draw() {
     
     background(255,255,255);
 
-for( x = 200 ; x > 100; x-- ){
+//for( x = 200 ; x > 100; x-- ){
 
     circle(300, 300, x );
     
     noFill();
    
 
-}
+// }
 
 }
