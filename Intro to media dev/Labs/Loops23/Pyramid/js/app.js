f2c43eca2a309fde1959674469a32d02a6328e26
Jvigar2000 <jameson.vigar@aol.com>
2023-07-07 19:45:39 -0400

    completed labs

--- Intro to media dev/Labs/Loops23/Pyramid/js/app.js
+++ Intro to media dev/Labs/Loops23/Pyramid/js/app.js
@@ -3,8 +3,10 @@
 // N220
 // "Pyramid"
 
+
 function setup(){
 
+    // Creating the canvas
     createCanvas(800,800)
 
 
@@ -12,9 +14,54 @@ function setup(){
 
 function draw(){
 
-    for(i = 0; i < 5; i++){
+    
+  // this initial code had it flipped and mirrored, resulting in me flipping the addition to subtraction 
+
+    /* for ( z = 0; z < 4; z++){
+            let x = z * 100;
+            let y = 100;
+
+        for ( i = 0; i < z; i++){
+
+        }
+        for ( i = 0; i <= z; i++){
+
+            fill(255,0,0);
+
+            square( x, y, 50 );
+            
+
+
+            y = y + 100;
+
+        }
 
         
+
+}   
+    */ 
+   // Loop 1 
+    for ( z = 4; z > 0; z--){
+
+        // creating the x varible to space each square apart 
+        let x = z * 50;
+
+        // the intial y position
+        let y =300;
+
+    // nested loop 
+    for ( i = 4; i >= z; i--){
+
+        // fill square red 
+        fill(255,0,0);
+
+        // create square 
+        square( x, y, 50 );
+        
+        // shift squares down 
+        y = y - 50;
+
+    }
 
     }
 
