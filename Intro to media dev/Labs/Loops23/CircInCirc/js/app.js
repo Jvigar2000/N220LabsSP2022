0bcd377e038632b2ba5e890ac326b007d8cc940f
Jvigar2000 <jameson.vigar@aol.com>
2023-07-07 13:48:42 -0400

    loops

---
+++ Intro to media dev/Labs/Loops23/CircInCirc/js/app.js
@@ -0,0 +1,15 @@
+// Jameson Vigar
+// 7/7/23
+// N220
+// "Circle in Circle"
+
+function setup(){
+    createCanvas(600, 600);
+    // backgrond black
+    background(0);
+
+}
+let x = 0;
+
+
+circle(30, 30, 20); 
