52d5c688efc4362be5a132ea5efbe715c569c287
Jvigar2000 <jameson.vigar@aol.com>
2023-07-07 20:36:50 -0400

    diameter

--- Intro to media dev/Labs/Loops23/CircInCirc/js/app.js
+++ Intro to media dev/Labs/Loops23/CircInCirc/js/app.js
@@ -6,20 +6,25 @@
 function setup() { 
       
     // Create Canvas of given size 
-    createCanvas(400,300); 
+    createCanvas(300,300); 
     // set canvas color to white 
-  background(255,255,255);
+  background(200);
 } 
   
 // draw function 
 function draw() { 
    
   // Create a loop that decreases
-  for(x =  200 ; x  > 0; x-- ){  
-    // Draw a circle that multiplies in size to create circles within circles
-    circle(200, 150, x * 16 ); 
-    //noFill();
+  for(x =  40 ; x  > 0; x-- ){  
+   
+    fill(255,255,255);
+     //noFill();
+      // Draw a circle that multiplies in size to create circles with circles inside 
+    circle(150, 150, x * 7.5 ); 
+
+    // the x multiplier was found through trial and error as well as the the original diameter 
+   
     
-    //fill(255,255,255);
+    
   }
 }
