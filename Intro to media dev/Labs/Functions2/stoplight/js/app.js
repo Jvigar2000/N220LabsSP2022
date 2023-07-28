b4c3c431e818e9aa7066d84e2da91891d5e205ae
Jvigar2000 <jameson.vigar@aol.com>
2023-07-28 12:10:21 -0400

    stoplight finished

--- Intro to media dev/Labs/Functions2/stoplight/js/app.js
+++ Intro to media dev/Labs/Functions2/stoplight/js/app.js
@@ -7,6 +7,10 @@ function setup(){
 
     // create canvas
     createCanvas(800, 800);
+    fill(0);
+    // set the x to align it in the middle, found through trial and error 
+    // made a rectangle to make it pretty 
+    rect(width/2.4, 100, 140, 400);
 
 }
  
@@ -16,20 +20,20 @@ function draw(){
     let colors = ['#FF0000','#FFFF00','#00FF00'];
 
     // set starting values 
-    x = 100
-    y = 100
-    r = 100
+    // x = 100;
+    y = 100;
+    r = 100;
 
     // loop to create the circles for each color 
     for(i=0; i < colors.length; i++){
 
         // position circles on top of each other
-        y = y + 100
+        y = y + 100;
 
         // fill with color from array
         fill(colors[i]);
-        // create circle
-        circle(x, y, r);
+        // create circle // decided to use width/2 instead of a x to place it in the center 
+        circle( width/2, y, r);
 
         //fill(colors[i]); // with fill after colors were out of order
         
