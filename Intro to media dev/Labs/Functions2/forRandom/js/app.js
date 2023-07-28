a68aaac06ae6ef8ff30e454dda5f23e665bb07be
Jvigar2000 <jameson.vigar@aol.com>
2023-07-28 15:23:33 -0400

    final codes

--- Intro to media dev/Labs/Functions2/forRandom/js/app.js
+++ Intro to media dev/Labs/Functions2/forRandom/js/app.js
@@ -3,3 +3,37 @@
 // N220
 // "Click For Random"
 
+
+//Array of four colors 
+let colors = ['#0000FF','#008000','	#FFBF00','#800080'];
+
+function setup() {
+  
+  // Create the canvas and the circle.
+  createCanvas(800, 800);
+ 
+  fill('0');
+  
+  circle(200,200,200);
+  
+}
+
+// function to change the color 
+function setColor(i) {
+  fill(i);
+  
+  circle(200,200,200);
+
+}
+
+// mouse clicked function
+function mouseClicked(){
+
+
+  // checks the mouses postion in comparison to the radius to see if the mouse is in the circle 
+ if (dist(mouseX, mouseY, 200, 200) <= 100){
+    setColor(colors[Math.floor(Math.random() * 4)]);
+  
+
+ }
+}
--- Intro to media dev/Labs/Functions2/isDivis/js/app.js
+++ Intro to media dev/Labs/Functions2/isDivis/js/app.js
@@ -2,3 +2,23 @@
 // 07/27/23
 // N220
 // "Is divisible by 7"
+
+
+// function to determine divisiblity 
+function isDivis(){
+    
+    // retrieves user input 
+    let x = document.getElementById("number").value;
+
+    
+    // if it is divisible it will display that it is true 
+    if(x % 7 == 0){
+
+        document.getElementById("answer").innerHTML= x + "number is divisible by 7";
+
+    }else{
+        // if it is fale it will display that it is false
+        document.getElementById("answer").innerHTML= x + " is not divisible by 7";
+    }
+
+}
