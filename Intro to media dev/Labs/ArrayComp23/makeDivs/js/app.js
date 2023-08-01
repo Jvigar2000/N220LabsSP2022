ac54a7abff1280e1f040b71e00d506b3280be123
Jvigar2000 <jameson.vigar@aol.com>
2023-08-01 18:21:52 -0400

    lab final

--- Intro to media dev/Labs/ArrayComp23/makeDivs/js/app.js
+++ Intro to media dev/Labs/ArrayComp23/makeDivs/js/app.js
@@ -3,7 +3,8 @@
 // N220
 // "Make Divs"
 
-
+// create divs 
+function createDiv(){
 
 // define object array 
 let objects = [
@@ -21,10 +22,16 @@ let objects = [
 
     // create a div from each of the objects 
     div = document.createElement("div");
-    div.setAttribute('color', objects[i].color);
+
+    // added text to see where my divs were, turns out this fixed my program too
+    div.innerText = "where are my divs ";
+    //div.setAttribute("color", objects[i].color);
+    div.style.background = objects[i].color ;
     div.setAttribute('height', objects[i].height);
     div.setAttribute('width', objects[i].width);
 
     // add it to the body
     document.body.appendChild(div);
+    
    }
+}
