5309dc5846959f26c9f6776b5ac052399b5080bd
Jvigar2000 <jameson.vigar@aol.com>
2023-08-05 10:18:56 -0400

    lab fix

--- Intro to media dev/Labs/Events/colorMixer/js/app.js
+++ Intro to media dev/Labs/Events/colorMixer/js/app.js
@@ -9,57 +9,62 @@ g = 0;
 
 function colorAdj(d){
 
- 
+ // get the color adjust ment code form the button
 adj = document.getElementById(d.id);
+// get the div 
 div = document.getElementById("main");
 
 
 
-if(adj == redOne){
+console.log(adj);
+
+// determine which color to alter 
+if(adj === redOne){
 
     r = r + 1;
-    div.style.backgroundColor = (r,g,b);
+    div.style.backgroundColor = rgb(r,g,b);
+    
 }
-else if(adj == redFive){
+else if(adj ===redFive){
 
     r = r + 5;
     div.style.backgroundColor = (r,g,b);
 
 }
-else if(adj == redTen){
+else if(adj ===redTen){
 
     r = r + 10;
     div.style.backgroundColor = (r,g,b);
 
 }
-else if(adj == blueOne){
+else if(adj === blueOne){
 
     b = b + 1;
     div.style.backgroundColor = (r,g,b);
 
 }
-else if(adj == blueFive){
+else if(adj === blueFive){
 
     b = b + 5;
     div.style.backgroundColor = (r,g,b);
 
-}else if(adj == blueTen){
+}else if(adj === blueTen){
 
     b = b + 10;
     div.style.backgroundColor = (r,g,b);
 
-}else if(adj == greenOne){
+}else if(adj === greenOne){
 
     g = g + 1;
     div.style.backgroundColor = (r,g,b);
 
 }
-else if(adj == greenFive){
+else if(adj === greenFive){
 
     g = g + 5;
     div.style.backgroundColor = (r,g,b);
 
-}else if(adj == greenTen){
+}else if(adj === greenTen){
 
     g = g + 10;
     div.style.backgroundColor = (r,g,b);
